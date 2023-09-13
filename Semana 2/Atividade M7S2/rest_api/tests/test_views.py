c63bf5c0fc65c09737eea7a43d4aeeea10f43f9f
RxbPythonDev <rxbpythondev@gmail.com>
2023-09-13 18:56:19 -0300

    Atividade da segunda semana do módulo 7

---
+++ Semana 2/Atividade M7S2/rest_api/tests/test_views.py
@@ -0,0 +1,71 @@
+import pytest
+import datetime
+from rest_framework.test import APIClient
+from model_bakery import baker
+from reserva.models import Petshop, Reserva
+from rest_api.serializers import PetshopModelSerializer
+
+@pytest.fixture
+def dados_agendamento():
+    hoje = datetime.date.today()
+    petshop = baker.make(Petshop)
+    return {
+        'nome': 'nome teste', 'email': 'email@email.com',
+        'nome_pet': 'pet teste', 'data': hoje, 'turno': 'manhã',
+        'tamanho': 0, 'observacoes': '', 'petshop': petshop.pk,
+    }
+
+@pytest.fixture
+def usuario():
+    return baker.make('auth.User')
+
+@pytest.mark.django_db
+def test_criar_agendamento(usuario, dados_agendamento):
+    cliente = APIClient()
+    cliente.force_authenticate(usuario)
+    resposta = cliente.post('/api/agendamento', dados_agendamento)
+    assert resposta.status_code == 201
+
+@pytest.fixture
+def agendamento():
+    return baker.make(Reserva, nome='reserva teste', nome_pet='pet teste')
+
+@pytest.mark.django_db
+def test_todos_agendamentos(agendamento):
+    cliente = APIClient()
+    resposta = cliente.get('/api/agendamento')
+    assert len(resposta.data['results']) == 1
+
+@pytest.fixture
+def petshops():
+    return baker.make(Petshop, _quantity=3)
+
+@pytest.mark.django_db
+def test_todos_petshops(petshops):
+    cliente = APIClient()
+    resposta = cliente.get('/api/petshop')
+    assert len(petshops) == 3
+    serializer = PetshopModelSerializer(Petshop.objects.all(), many=True)
+    assert resposta.data['results'] == serializer.data
+
+@pytest.mark.django_db
+def test_consultar_agendamento_especifico(agendamento):
+    cliente = APIClient()
+    resposta = cliente.get('/api/agendamento/1')
+    assert resposta.data['nome'] == 'reserva teste'
+
+@pytest.mark.django_db
+def test_atualizar_agendamento_especifico(agendamento, usuario):
+    cliente = APIClient()
+    cliente.force_authenticate(usuario)
+    resposta = cliente.patch('/api/agendamento/1', {'nome': 'reserva teste atualizada'})
+    assert resposta.status_code == 200
+    assert resposta.data['nome'] == 'reserva teste atualizada' and resposta.data['nome_pet'] == 'pet teste'
+
+@pytest.mark.django_db
+def test_remover_agendamento_especifico(agendamento, usuario):
+    cliente = APIClient()
+    cliente.force_authenticate(usuario)
+    resposta = cliente.delete('/api/agendamento/1')
+    assert resposta.status_code == 204
+    assert len(cliente.get('/api/agendamento').data['results']) == 0
